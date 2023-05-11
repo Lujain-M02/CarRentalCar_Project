23ee953e05be65988628b70c136b4161e0da0f2e
Lujain-M02 <ilujain321@gmail.com>
2023-05-12 01:32:01 +0300

    log out buttons

--- app/src/main/java/com/example/carrental/More_page.java
+++ app/src/main/java/com/example/carrental/More_page.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class More_page extends AppCompatActivity {
     Button btn_myCars;
+    ImageButton btn_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,7 @@ public class More_page extends AppCompatActivity {
         setContentView(R.layout.activity_more_page);
 
         btn_myCars=findViewById(R.id.btn_myCars);
-
+        btn_logout=findViewById(R.id.btn_logout);
 
 
         btn_myCars.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +30,13 @@ public class More_page extends AppCompatActivity {
         });
 
 
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Home.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
