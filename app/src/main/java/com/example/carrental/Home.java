45b36a6024418b174d9052d788e7ffb74d56d47c
Lujain-M02 <ilujain321@gmail.com>
2023-05-12 00:37:45 +0300

    Home activity buttons

--- app/src/main/java/com/example/carrental/Home.java
+++ app/src/main/java/com/example/carrental/Home.java
@@ -1,14 +1,38 @@
 package com.example.carrental;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+    Button btn_singin,btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btn_singin=findViewById(R.id.btn_singin);
+        btn_login=findViewById(R.id.btn_login);
+
+        btn_singin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
